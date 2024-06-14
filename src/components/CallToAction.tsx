f00bb5fc94478c87b4867421d092cf9e303565fb
sgautier-dev <sgautier.dev@gmail.com>
2024-06-14 17:19:05 +0400

    cta done

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -21,14 +21,14 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Get started today
+            Saisissez cette opportunité
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+            Faites appel à un directeur externalisé pour des conseils
+            stratégiques et opérationnels.
           </p>
           <Button href="/register" color="white" className="mt-10">
-            Get 6 months free
+            Prenez rendez-vous dès aujourd&apos;hui
           </Button>
         </div>
       </Container>
